f1f7915883eca124fa596d0adbb6e16c7a6f323b
ben_son <136886788+ben-24-0@users.noreply.github.com>
2025-10-01 02:56:22 +0530

    app.locale var & res.local.var

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 const express = require("express");
 const app = express();
-const path = require("path");
+const port = 3000;
 
+// Set view engine to EJS
 app.set("view engine", "ejs");
-app.set("views", "./views");
-app.use("/login", express.static(path.join(__dirname, "..", "public")));
-// app.use(express.static(path.join(__dirname,'public')))
 
-app.use("/", (req, res) => {
-  res.send("hello");
+// App-wide locals
+app.locals.siteTitle = "My Awesome Site";
+
+// Middleware to set request-specific locals
+app.use((req, res, next) => {
+  res.locals.username = "Alice"; // changes per request
+  next();
 });
-// app.use("/", (req, res) => {
-//   console.log(path.join(__dirname, "..", "public"));
-//   res.sendFile(path.join(__dirname, "..", "public","index.html"));
-// });
-app.listen(7777, () => {
-  console.log("listening");
+
+// Route handler
+app.get("/", (req, res) => {
+  res.render("index"); // res.locals and app.locals are both available in template
+});
+
+// Another route with different username
+app.get("/bob", (req, res, next) => {
+  res.locals.username = "Bob";
+  next();
+}, (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
