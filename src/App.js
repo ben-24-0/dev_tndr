e5fe73da47bf8e8e5b0ef6bf0bad88fa5af3893d
ben_son <136886788+ben-24-0@users.noreply.github.com>
2025-10-01 02:34:35 +0530

    sdendFile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-// Multiple options
-app.get(/^\/(cat|dog|bird)$/, (req, res) => res.send("Animal route"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use("/login", express.static(path.join(__dirname, "..", "public")));
+// app.use(express.static(path.join(__dirname,'public')))
 
-app.get("/hello", (req, res) => {
-  res.send("case senstive");
+app.use("/", (req, res) => {
+  res.send("hello");
 });
-app.get(/^\/hello$/i, (req, res) => res.send("Hello (case insensitive)"));
-
-// Numbers only
-app.get(/^\/product\/[0-9]+$/, (req, res) => res.send("Product route"));
-// Matches: /product/123 but not /product/abc
-
-app.get(/^\/ab?cd$/, (req, res) => {
-  res.send("matched abcd or acd");
-});
-
-app.get(/^\/a(bc)?d$/, (req, res) => {
-  res.send({ firstName: "Akshay", lastName: "SAINI" });
-});
+// app.use("/", (req, res) => {
+//   console.log(path.join(__dirname, "..", "public"));
+//   res.sendFile(path.join(__dirname, "..", "public","index.html"));
+// });
 app.listen(7777, () => {
   console.log("listening");
 });
